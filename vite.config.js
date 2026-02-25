a5d63211ec03c5ddaa43fd10df60bc2c36108994
Vikram Belde <symmetriccurve@gmail.com>
2026-02-25 23:16:00 +0530

    Update vite base config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // important for deploying to github pages directly without exact repo path
+  base: '/licforyou/', // important for deploying to github pages directly without exact repo path
 })
